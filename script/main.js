4165ced223146fca51396f08ed162de9603d0b8b
unknown <si620446@gmail.com>
2019-11-22 20:16:15 +0900

    바탕 애니메이션 추가

--- script/main.js
+++ script/main.js
@@ -1,24 +1,44 @@
 $(function () {
     var count = 0;
+    var scrollValue = $('html, body').offset().top;
     var $next_page = $('#second').offset().top;
+    var $page_length = $('.inner_wrapper li').length;
+    console.log($page_length);
     console.log($next_page);
 
     $('.click_bottom').click(function (e) {
-        count++;
+        if(count<$page_length){
+            count++;
+        }
+        
         e.preventDefault();
         $('html,body').animate({
             scrollTop: $next_page * count + 'px'
         }, 400)
         console.log(count);
+        console.log(scrollValue);
     });
 
     $('.click_top').click(function (e) {
-        count--;
+        if(count>0){
+            count--;
+        }
+        
         e.preventDefault();
         $('html,body').animate({
             scrollTop: $next_page * count + 'px'
         }, 400)
         console.log(count);
+        console.log(scrollValue);
     });
+    
+    $(window).scroll(function(){
+        if($(window).scrollTop()==$next_page){
+            $('#second > .default').animate({
+                opacity:1
+            })
+        }
+    });
+    
 
 });
